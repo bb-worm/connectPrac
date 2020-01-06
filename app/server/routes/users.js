b01c31def61a1167c0f3eb973ee385339ca86f7c
bb-worm <xofyd99@naver.com>
2020-01-06 15:05:23 +0900

    client by expo

--- app/server/routes/users.js
+++ app/server/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendfile(path.resolve('public/images/qu.jpeg'));
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
